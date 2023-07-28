53b696a4f6ef782d7cb6c38a2357c7310160a03c
mecovibre <132189212+mecovibre@users.noreply.github.com>
2023-07-28 12:37:51 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,8 +11,8 @@ const BLOG = {
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2023, // If leave this empty, current year will be used.
-  postsPerPage: 10,
+  since: 2021, // If leave this empty, current year will be used.
+  postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
