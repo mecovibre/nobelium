0951bbe785b3e195c1e4da323a9ecc3341ad33dc
mecovibre <132189212+mecovibre@users.noreply.github.com>
2023-07-28 14:02:15 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'MECO on the Rock',
+  title: 'MECO',
   author: 'MECO',
   email: 'meco.vibre@gmail.com',
   link: 'https://mecovibre.vercel.app',
-  description: '그냥 이것저것.',
+  description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Seoul', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
