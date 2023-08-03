9c627276742bcf7262c954de046117bbf8c5e808
mecovibre <132189212+mecovibre@users.noreply.github.com>
2023-08-03 12:16:32 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '5a35a72c-143a-47b7-9bca-69452d0b9b16', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
